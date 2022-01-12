09070337e1ff4f3621e3255f42281059c5e2dd7a
onurk03 <65676529+onurk03@users.noreply.github.com>
2022-01-12 14:45:45 -0600

    Implemented Back-End for Adding Items to Cart
    Implemented a JS backend so that items can be added to the cart as objects with name, price, and quantity values. The amount of items added to the cart will pop up on the top-right of the cart icon as a bubble. Next thing to do is to implement the check-out page so that the items that are added to the cart can be displayed properly for a check-out system.

--- scripts/OrderMenu.js
+++ scripts/OrderMenu.js
@@ -1,11 +1,11 @@
 // Collapsible Menu Implementation
-var collapsibleMenu = document.getElementsByClassName("collapsible");
-var i;
+let collapsibleMenu = document.getElementsByClassName("collapsible");
+let i;
 
 for(i = 0; i < collapsibleMenu.length; i++) {
     collapsibleMenu[i].addEventListener("click",function() {
         this.classList.toggle("collapsed");
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
         if(content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
@@ -13,3 +13,46 @@ for(i = 0; i < collapsibleMenu.length; i++) {
         }
     });
 }
+// Cart Implementation
+class menuItem {
+    constructor(name,price,quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+let menuItems = [
+    "Grilled Cheese","Avocado Toast", "BLT", "Tuna", "Turkey", "Club",
+    "French Fries", "Cheese Curds", "Pretzel Bites", "Boneless Wings",
+    "Cafe Mocha", "Cafe Latte", "Cappuccino", "Americano", "Double Espresso",
+    "Soda","Iced Tea","Orange Juice", "Bottled Water",
+    "Tiramisu","Ice Cream", "New York Cheesecake", "Fudge Brownie"
+]
+
+let prices = [
+    4.99,6.99,5.99,4.99,4.99,5.99,
+    2.99,3.99,2.99,6.99,
+    3.95,3.95,2.95,2.95,2.95,
+    1.99,2.99,3.99,1.50,
+    5.99,2.99,6.99,5.99
+]
+
+let cart = [];
+
+let cartQuantity = document.getElementById("cart-quantity");
+let cartCount = 0;
+
+let addToCart = document.getElementsByClassName("add-cart");
+for(let i = 0; i < addToCart.length ; i++) {
+    addToCart[i].addEventListener("click",  function() {
+        let quantities = document.getElementsByClassName("item-quantity");
+        let item = new menuItem(menuItems[i], prices[i], quantities[i].value);
+        cart.push(item);
+        cartCount += parseInt(quantities[i].value);
+        cartQuantity.innerHTML = cartCount;
+        cartQuantity.style.display = "inline";
+    });
+}
+
+console.log(cart);
