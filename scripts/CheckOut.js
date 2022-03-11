e48ee17483e84728b3a8b31658b2ae07407ad28e
onurk03 <65676529+onurk03@users.noreply.github.com>
2022-03-11 12:08:20 -0600

    Item Removal & Unnecessary Menu Page Removed

--- scripts/CheckOut.js
+++ scripts/CheckOut.js
@@ -5,7 +5,7 @@ let processing = document.querySelector(".processing");
 let checkIcon = document.querySelector(".check-icon");
 let resultText = document.querySelector(".result-text");
 
-checkOut.addEventListener("click", function () {
+checkOut.addEventListener("click", () => {
     checkOutBox.style.display = "none";
     processing.style.display = "inline-block";
     setTimeout(function () {
@@ -20,7 +20,23 @@ if(localStorage.length > 1) {
 
 for(let i = 0; i < cart.length; i++) {
     document.querySelector(".items-box").innerHTML +=
-        `<p class="check-out-item"> ${cart[i].name} <em> $${cart[i].price}</em>
-         <em class="item-quantity">${cart[i].quantity} ct.</em></em>`;
+        `<p class="check-out-item no${i + 1}"> ${cart[i].name} 
+            <em> $${cart[i].price}</em>
+            <em class="item-quantity">${cart[i].quantity} ct.</em>
+            <button class="remove-item no${i + 1}">x</button>
+        </p>`;
 }
+
+
+let removeButton = document.querySelectorAll(".remove-item");
+
+removeButton.forEach((button) =>
+    button.addEventListener("click", () => {
+    let index = "." + button.classList[1];
+    cart.splice(index,1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("cartCount", parseInt(localStorage.getItem("cartCount")) - 1);
+    document.querySelector(index).remove();
+}));
+
 
